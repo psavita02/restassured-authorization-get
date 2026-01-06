7457dd86d4bd44065ebe569769fa6c4d8e36ad0b
psavita02 <priyanka.savita@telusinternational.com>
2026-01-06 20:04:01 +0530

    myfirstcommit

--- Ex01_HandleOutput.java
+++ Ex01_HandleOutput.java
@@ -7,14 +7,9 @@ public static void main(String[] args) {
 System.out.println(1);
 System.out.println(1.6);
 System.out.println('A');
-System.out.println("Hello World");
+System.out.println("Hi World");
 System.out.println(true);
 System.out.println("******************************");
-System.out.print(1);
-System.out.print(1.6);
-System.out.print('A');
-System.out.print("Hello World");
-System.out.print(true);
 }
 }
 
