1b734f2c8f2ac326624ae3ba1612ffda6aa14b5f
psavita02 <priyanka.savita@telusinternational.com>
2026-01-06 17:29:53 +0530

    Public Repo Created

--- Ex01_HandleOutput.java
+++ Ex01_HandleOutput.java
@@ -5,15 +5,16 @@ public class Ex01_HandleOutput
 {
 public static void main(String[] args) {
 System.out.println(1);
-System.out.println(1.5);
+System.out.println(1.6);
 System.out.println('A');
 System.out.println("Hello World");
 System.out.println(true);
 System.out.println("******************************");
 System.out.print(1);
-System.out.print(1.5);
+System.out.print(1.6);
 System.out.print('A');
 System.out.print("Hello World");
 System.out.print(true);
 }
 }
+
