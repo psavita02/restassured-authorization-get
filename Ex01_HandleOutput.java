c919b0d208088ce32a2bfe3a07cba0dffbe14202
psavita02 <priyanka.savita@telusinternational.com>
2026-01-06 17:17:31 +0530

    Add files via upload

---
+++ Ex01_HandleOutput.java
@@ -0,0 +1,19 @@
+
+package _01_basic;
+
+public class Ex01_HandleOutput 
+{
+public static void main(String[] args) {
+System.out.println(1);
+System.out.println(1.5);
+System.out.println('A');
+System.out.println("Hello World");
+System.out.println(true);
+System.out.println("******************************");
+System.out.print(1);
+System.out.print(1.5);
+System.out.print('A');
+System.out.print("Hello World");
+System.out.print(true);
+}
+}
